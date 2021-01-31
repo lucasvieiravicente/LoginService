be791524b7530d9a8c17055addcd73342f60f274
Lucas Vieira Vicente <lucasvieiravicente1@gmail.com>
2021-01-31 16:45:31 -0300

    construtores base

--- Domain/Models/BaseEntity.cs
+++ Domain/Models/BaseEntity.cs
@@ -4,9 +4,28 @@ namespace LoginService.Domain.Models
 {
     public class BaseEntity
     {
+        #region [Constructors]
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            FlagActive = true;
+            CreatedAt = DateTime.Now;
+            ModifiedAt = DateTime.Now;
+        }
+
+        public BaseEntity(Guid id, DateTime createdAt, DateTime modifiedAt)
+        {
+            Id = id;
+            CreatedAt = createdAt;
+            ModifiedAt = modifiedAt;
+        }
+        #endregion
+
+        #region [Columns]
         public Guid Id { get; set; }
         public bool FlagActive { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
+        #endregion
     }
 }
